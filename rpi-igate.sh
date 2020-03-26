b9880b2b175288baed5c3e1694c5b902bb07e155
Squeegee16 <20779713+Squeegee16@users.noreply.github.com>
2020-03-26 15:12:33 -0300

    Update rpi-igate.sh

--- rpi-igate.sh
+++ rpi-igate.sh
@@ -1,8 +1,14 @@
 #! /bin
 # https://www.youtube.com/watch?v=g_AoAGjQMl8&feature=youtu.be
+sudo apt-get update -y
+sudo apt-get install git
+
 ## RTl-SDR
-sudo apt-get update
-sudo apt-get install cmake build-essential libusb-1.0-0-dev -y
+echo 'blacklist dvb_usb_rtl28xxu' >no-rtl.conf
+echo 'blacklist rtl2832' >>no-rtl.conf
+echo' blacklist rtl2830' >>no-rtl.conf
+
+sudo mv no-rtl.conf /etc/modprobe.d/
 cd ~
 git clone git://git.osmocom.org/rtl-sdr.git
 cd rtl-sdr
@@ -12,8 +18,8 @@ cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON
 make
 sudo make install
 sudo ldconfig
-
-
+cd ~
+sudo cp rtl-sdr/rtl-sdr.rules /etc/udev/rules.d/
 
 ## direwolf
 sudo apt-get remove --purge pulseaudio
