736d0999d78761f3a22facde80fa0fda19f4a256
Squeegee16 <20779713+Squeegee16@users.noreply.github.com>
2020-03-30 12:19:41 -0300

    Update cubicSdr.sh

--- cubicSdr.sh
+++ cubicSdr.sh
@@ -8,7 +8,6 @@ sudo apt-get install libpulse-dev libgtk-3-dev -y
 echo "getting SoapySDR"
 git clone https://github.com/pothosware/SoapySDR.git
 cd SoapySDR
-mkdir build
 cd build
 cmake ../ -DCMAKE_BUILD_TYPE=Release
 make -j4
@@ -29,7 +28,6 @@ sudo ldconfig
 cd ..
 ###wxwidgets
 echo "getting wxWidgets"
-mkdir wxwidgets
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/wxWidgets-3.1.3.tar.bz2
 tar -xvjf wxWidgets-3.1.3.tar.bz2
 rm wxWidgets-3.1.3.tar.bz2
@@ -42,7 +40,7 @@ cd ~/applications/cubic
 ##cubic SDR
 echo "getting cubicsdr"
 git clone https://github.com/cjcliffe/CubicSDR.git
-cd cubicsdr
+cd CubicSDR
 mkdir build
 cd build
 cmake ../ -DCMAKE_BUILD_TYPE=Release -DwxWidgets_CONFIG_EXECUTABLE=~/Develop/wxWidgets-staticlib/bin/wx-config -DUSE_HAMLIB=1
